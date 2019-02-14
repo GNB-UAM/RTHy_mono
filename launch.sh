2eb8a7cd70ac64a0e494b48df08e25930b842551
Manuel Reyes <mnrs94@gmail.com>
2019-02-14 19:20:19 +0100

    gh

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 echo -e '#!/bin/bash
-#$ -N OB
+#$ -N RTHy_mono
 #$ -cwd
-#$ -o OB.$JOB_ID.out
-#$ -e OB.$JOB_ID.err
+#$ -o jobs/OB.$JOB_ID.out
+#$ -e jobs/OB.$JOB_ID.err
 
 /bin/echo Estoy corriendo en el nodo  `hostname`
 
@@ -93,7 +93,7 @@ python invariante.py -f data_out/GH_1/res_GH_20.00_0.95.txt -n1 20.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -187,7 +187,7 @@ python invariante.py -f data_out/GH_1/res_GH_21.00_0.95.txt -n1 21.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -281,7 +281,7 @@ python invariante.py -f data_out/GH_1/res_GH_22.00_0.95.txt -n1 22.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -375,7 +375,7 @@ python invariante.py -f data_out/GH_1/res_GH_23.00_0.95.txt -n1 23.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -469,7 +469,7 @@ python invariante.py -f data_out/GH_1/res_GH_24.00_0.95.txt -n1 24.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -563,7 +563,7 @@ python invariante.py -f data_out/GH_1/res_GH_25.00_0.95.txt -n1 25.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -657,7 +657,7 @@ python invariante.py -f data_out/GH_1/res_GH_26.00_0.95.txt -n1 26.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -751,7 +751,7 @@ python invariante.py -f data_out/GH_1/res_GH_27.00_0.95.txt -n1 27.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -845,7 +845,7 @@ python invariante.py -f data_out/GH_1/res_GH_28.00_0.95.txt -n1 28.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -939,7 +939,7 @@ python invariante.py -f data_out/GH_1/res_GH_29.00_0.95.txt -n1 29.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1033,7 +1033,7 @@ python invariante.py -f data_out/GH_1/res_GH_30.00_0.95.txt -n1 30.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1127,7 +1127,7 @@ python invariante.py -f data_out/GH_1/res_GH_31.00_0.95.txt -n1 31.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1221,7 +1221,7 @@ python invariante.py -f data_out/GH_1/res_GH_32.00_0.95.txt -n1 32.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1315,7 +1315,7 @@ python invariante.py -f data_out/GH_1/res_GH_33.00_0.95.txt -n1 33.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1409,7 +1409,7 @@ python invariante.py -f data_out/GH_1/res_GH_34.00_0.95.txt -n1 34.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1503,7 +1503,7 @@ python invariante.py -f data_out/GH_1/res_GH_35.00_0.95.txt -n1 35.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1597,7 +1597,7 @@ python invariante.py -f data_out/GH_1/res_GH_36.00_0.95.txt -n1 36.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1691,7 +1691,7 @@ python invariante.py -f data_out/GH_1/res_GH_37.00_0.95.txt -n1 37.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1785,7 +1785,7 @@ python invariante.py -f data_out/GH_1/res_GH_38.00_0.95.txt -n1 38.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1879,7 +1879,7 @@ python invariante.py -f data_out/GH_1/res_GH_39.00_0.95.txt -n1 39.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -1973,7 +1973,7 @@ python invariante.py -f data_out/GH_1/res_GH_40.00_0.95.txt -n1 40.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2067,7 +2067,7 @@ python invariante.py -f data_out/GH_1/res_GH_41.00_0.95.txt -n1 41.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2161,7 +2161,7 @@ python invariante.py -f data_out/GH_1/res_GH_42.00_0.95.txt -n1 42.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2255,7 +2255,7 @@ python invariante.py -f data_out/GH_1/res_GH_43.00_0.95.txt -n1 43.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2349,7 +2349,7 @@ python invariante.py -f data_out/GH_1/res_GH_44.00_0.95.txt -n1 44.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2443,7 +2443,7 @@ python invariante.py -f data_out/GH_1/res_GH_45.00_0.95.txt -n1 45.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2537,7 +2537,7 @@ python invariante.py -f data_out/GH_1/res_GH_46.00_0.95.txt -n1 46.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2631,7 +2631,7 @@ python invariante.py -f data_out/GH_1/res_GH_47.00_0.95.txt -n1 47.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2725,7 +2725,7 @@ python invariante.py -f data_out/GH_1/res_GH_48.00_0.95.txt -n1 48.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2819,7 +2819,7 @@ python invariante.py -f data_out/GH_1/res_GH_49.00_0.95.txt -n1 49.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -2913,7 +2913,7 @@ python invariante.py -f data_out/GH_1/res_GH_50.00_0.95.txt -n1 50.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3007,7 +3007,7 @@ python invariante.py -f data_out/GH_1/res_GH_51.00_0.95.txt -n1 51.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3101,7 +3101,7 @@ python invariante.py -f data_out/GH_1/res_GH_52.00_0.95.txt -n1 52.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3195,7 +3195,7 @@ python invariante.py -f data_out/GH_1/res_GH_53.00_0.95.txt -n1 53.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3289,7 +3289,7 @@ python invariante.py -f data_out/GH_1/res_GH_54.00_0.95.txt -n1 54.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3383,7 +3383,7 @@ python invariante.py -f data_out/GH_1/res_GH_55.00_0.95.txt -n1 55.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3477,7 +3477,7 @@ python invariante.py -f data_out/GH_1/res_GH_56.00_0.95.txt -n1 56.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3571,7 +3571,7 @@ python invariante.py -f data_out/GH_1/res_GH_57.00_0.95.txt -n1 57.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3665,7 +3665,7 @@ python invariante.py -f data_out/GH_1/res_GH_58.00_0.95.txt -n1 58.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3759,7 +3759,7 @@ python invariante.py -f data_out/GH_1/res_GH_59.00_0.95.txt -n1 59.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3853,7 +3853,7 @@ python invariante.py -f data_out/GH_1/res_GH_60.00_0.95.txt -n1 60.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -3947,7 +3947,7 @@ python invariante.py -f data_out/GH_1/res_GH_61.00_0.95.txt -n1 61.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4041,7 +4041,7 @@ python invariante.py -f data_out/GH_1/res_GH_62.00_0.95.txt -n1 62.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4135,7 +4135,7 @@ python invariante.py -f data_out/GH_1/res_GH_63.00_0.95.txt -n1 63.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4229,7 +4229,7 @@ python invariante.py -f data_out/GH_1/res_GH_64.00_0.95.txt -n1 64.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4323,7 +4323,7 @@ python invariante.py -f data_out/GH_1/res_GH_65.00_0.95.txt -n1 65.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4417,7 +4417,7 @@ python invariante.py -f data_out/GH_1/res_GH_66.00_0.95.txt -n1 66.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4511,7 +4511,7 @@ python invariante.py -f data_out/GH_1/res_GH_67.00_0.95.txt -n1 67.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4605,7 +4605,7 @@ python invariante.py -f data_out/GH_1/res_GH_68.00_0.95.txt -n1 68.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4699,7 +4699,7 @@ python invariante.py -f data_out/GH_1/res_GH_69.00_0.95.txt -n1 69.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4793,7 +4793,7 @@ python invariante.py -f data_out/GH_1/res_GH_70.00_0.95.txt -n1 70.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4887,7 +4887,7 @@ python invariante.py -f data_out/GH_1/res_GH_71.00_0.95.txt -n1 71.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -4981,7 +4981,7 @@ python invariante.py -f data_out/GH_1/res_GH_72.00_0.95.txt -n1 72.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -5075,7 +5075,7 @@ python invariante.py -f data_out/GH_1/res_GH_73.00_0.95.txt -n1 73.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -5169,7 +5169,7 @@ python invariante.py -f data_out/GH_1/res_GH_74.00_0.95.txt -n1 74.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -5263,7 +5263,7 @@ python invariante.py -f data_out/GH_1/res_GH_75.00_0.95.txt -n1 75.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -5357,7 +5357,7 @@ python invariante.py -f data_out/GH_1/res_GH_76.00_0.95.txt -n1 76.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -5451,7 +5451,7 @@ python invariante.py -f data_out/GH_1/res_GH_77.00_0.95.txt -n1 77.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
@@ -5545,7 +5545,7 @@ python invariante.py -f data_out/GH_1/res_GH_78.00_0.95.txt -n1 78.00 -n2 0.95 -
 ' | qsub
 
 echo -e '#!/bin/bash
-#$ -N RTHybrid_mono
+#$ -N RTHy_mono
 #$ -cwd
 #$ -o jobs/OB.$JOB_ID.out
 #$ -e jobs/OB.$JOB_ID.err
