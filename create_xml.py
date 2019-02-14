6e5f34dfa7179c448a04985fd8a7ecdd12520ec8
Manuel Reyes <mnrs94@gmail.com>
2019-02-14 18:38:34 +0100

    gh

--- create_xml.py
+++ create_xml.py
@@ -6,7 +6,7 @@ variable_b = [x * 0.05 for x in range(0,  20)]
 #variable_a = ['40','38.3','36.5', '35', '33.3', '31.6', '30', '28.3', '26.6', '25', '23.3', '21.6', '20']
 #variable_b = ['0.0','0.01','0.02','0.03','0.04','0.05','0.06','0.07','0.08','0.09','0.1']
 
-entrada    = 'data/2018y_2m_20d/19h_18m_46s_1.txt'
+entrada    = 'data_in/2018y_2m_20d/19h_18m_46s_1.txt'
 xml        = 'xml_in/GH_1/xml_GH_'#+something
 salida     = 'data_out/GH_1/res_GH_'#+something
 file_R2 = 'salida_14feb19_'
