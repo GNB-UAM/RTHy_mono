d7cd12b5723856482f7ba409cf8c9ed68b424df5
Manuel Reyes <mnrs94@gmail.com>
2019-02-21 19:03:10 +0100

    ???

--- invariante.py
+++ invariante.py
@@ -17,7 +17,7 @@ args  = aux.arguments()
 
 data1 = aux.DataStruct1(args)
 t_ignore = args.freq * 12
-t_ignore_fin = args.freq * 10
+t_ignore_fin = args.freq * 6
 
 #version = 'one_test'
 #version = 'multiple'
@@ -108,24 +108,29 @@ delay_plot = delay
 #######
 print(args.n1 + ' and ' + args.n2)
 
-slope, intercept, r_value, p_value, std_err = stats.linregress(periodoLP, PDtoLP_plot)
-print( "Blue:    R²={:.2f}".format(r_value **2*100) + "%    m={:.3f}".format(slope ) )
-slope2, intercept2, r_value2, p_value2, std_err2 = stats.linregress(periodoLP, delay_plot)
-print( "Red:     R²={:.2f}".format(r_value2**2*100) + "%    m={:.3f}".format(slope2) )
+slope, intercept, r_value, p_value, std_err = stats.linregress(periodoLP, PDtoLP)
+print( "Blue:      R²={:.2f}".format(r_value **2*100) + "%    m={:.3f}".format(slope ) )
+slope2, intercept2, r_value2, p_value2, std_err2 = stats.linregress(periodoLP, delay)
+print( "Red:       R²={:.2f}".format(r_value2**2*100) + "%    m={:.3f}".format(slope2) )
+
+slope3, intercept3, r_value3, p_value3, std_err3 = stats.linregress(periodoLP, PDtoLP_raro)
+print( "Blue+1:    R²={:.2f}".format(r_value3**2*100) + "%    m={:.3f}".format(slope3 ) )
+slope4, intercept4, r_value4, p_value4, std_err4 = stats.linregress(periodoLP, delay_raro)
+print( "Red+1:     R²={:.2f}".format(r_value4**2*100) + "%    m={:.3f}".format(slope4) )
+
 
 #######
 # PLOT
 #######
 if args.plot_inv==True:
-	#early = 15
 	plt.plot(periodoLP, intercept + slope*np.asarray(periodoLP), alpha=0.5)
 	label = '1stLP to 1stDP    R²={:.2f}'.format(r_value **2*100) + '%    m={:.3f}'.format(slope )
-	plt.scatter(periodoLP, PDtoLP_plot, label=label, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Blues'))
+	plt.scatter(periodoLP, PDtoLP, label=label, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Blues'))
 	plt.colorbar()
 
 	plt.plot(periodoLP, intercept2 + slope2*np.asarray(periodoLP), alpha=0.5)
 	label = 'With delay          R²={:.2f}'.format(r_value2 **2*100) + '%    m={:.3f}'.format(slope2 )
-	plt.scatter(periodoLP, delay_plot, label=label, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Reds'))
+	plt.scatter(periodoLP, delay, label=label, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Reds'))
 	plt.colorbar()
 
 	plt.scatter(periodoLP, durationModel, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Greens'))
@@ -138,7 +143,32 @@ if args.plot_inv==True:
 	plt.legend(loc=1, framealpha=1.0)
 	plt.tight_layout()
 	plt.show()
+
+	############
+
+	plt.plot(periodoLP, intercept3 + slope3*np.asarray(periodoLP), alpha=0.5)
+	label = '1stLP to 1stDP    R²={:.2f}'.format(r_value3 **2*100) + '%    m={:.3f}'.format(slope3 )
+	plt.scatter(periodoLP, PDtoLP_raro, label=label, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Blues'))
+	plt.colorbar()
+
+	plt.plot(periodoLP, intercept4 + slope4*np.asarray(periodoLP), alpha=0.5)
+	label = 'With delay          R²={:.2f}'.format(r_value4 **2*100) + '%    m={:.3f}'.format(slope4 )
+	plt.scatter(periodoLP, delay_raro, label=label, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Reds'))
+	plt.colorbar()
+
+	plt.scatter(periodoLP, durationModel, c=np.linspace(0, len(periodoLP), len(periodoLP)), s=5, cmap=plt.get_cmap('Greens'))
+	plt.colorbar()
+
+	plt.xlabel("Period")
+	plt.ylabel("")
+
+	plt.title("Invariant")
+	plt.legend(loc=1, framealpha=1.0)
+	plt.tight_layout()
+	plt.show()
+
 else:
 	f_plot = open(args.name, 'a')
-	f_plot.write(args.n1 + ' ' + args.n2 + ' {:.5f}'.format(r_value**2*100) + ' {:.5f}'.format(r_value2**2*100) + ' {:.5f}'.format(slope) + ' {:.5f}'.format(slope2) + '\n')
+	# n1 n2 r_azul r_rojo s_azul s_roja rara_azul rara_roja srara_azul srara_roja
+	f_plot.write(args.n1 + ' ' + args.n2 + ' {:.5f}'.format(r_value**2*100) + ' {:.5f}'.format(r_value2**2*100) + ' {:.5f}'.format(slope) + ' {:.5f}'.format(slope2) +  ' {:.5f}'.format(r_value3**2*100) + ' {:.5f}'.format(r_value4**2*100) + ' {:.5f}'.format(slope3) + ' {:.5f}'.format(slope4) + '\n')
 	f_plot.close()
