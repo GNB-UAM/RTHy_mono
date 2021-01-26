8289b1ef447d2fc8b596150bf00595b56f63fbf3
Manuel Reyes <mnrs94@gmail.com>
2021-01-26 14:04:14 +0100

    a

--- genetico.py
+++ genetico.py
@@ -30,7 +30,7 @@ for individuo in population:
 	print(name)
 	os.system("./RTHybrid -xml "+name)
 	os.system("python invariante.py -f "+salida+" -n1 "+str(individuo.var_a)+" -n2 "+str(individuo.var_b)+" -n "+"result.txt")
-	with open('result.txt', 'r') as result:
+	with open('result.txt', 'r') as f:
 		last_line = f.read().splitlines()[-1]
 		individuo.r2 = last_line[2]
 
