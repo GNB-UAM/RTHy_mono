f5f8bec580af4c0977120c5fd45d52ee758ff5b9
Manuel Reyes <mnrs94@gmail.com>
2021-01-27 12:09:00 +0100

    a

--- genetico.py
+++ genetico.py
@@ -4,48 +4,71 @@ import os
 
 # Clase individuo
 class Individuo:
-	def __init__(self, id):
-		self.id = id
+	def __init__(self):
 		self.var_a = rd.uniform(15, 60)
 		self.var_b = rd.uniform(0, 20)
 		self.r2 = 0
 
 # Create initial population
 population_n = 10
+gen_n = 5
 population = []
 population_new = []
 for i in range(population_n):
-	population.append(Individuo(i))
-	population_new.append(Individuo(i))
+	population.append(Individuo())
+	population_new.append(Individuo())
 
 for individuo in population:
-	print("Individuo id = " + str(individuo.id))
+	#print("Individuo")
 	#print("Gen 1 = " + str(individuo.var_a))
 	#print("Gen 2 = " + str(individuo.var_b))
 
+for gen_i in range(gen_n):
 
-# Fitness function (execute simulation and save R2)
-for individuo in population:
-	name, salida = xml.create_xml(individuo.var_a, individuo.var_b)
-	print(name)
-	os.system("./RTHybrid -xml "+name)
-	os.system("python invariante.py -f "+salida+" -n1 "+str(individuo.var_a)+" -n2 "+str(individuo.var_b)+" -n "+"result.txt")
-	with open('result.txt', 'r') as f:
-		last_line = f.read().splitlines()[-1]
-		last_line = last_line.split(' ')
-		individuo.r2 = last_line[2]
+	# Fitness function (execute simulation and save R2)
+	for individuo in population:
+		name, salida = xml.create_xml(individuo.var_a, individuo.var_b)
+		print(name)
+		os.system("./RTHybrid -xml "+name)
+		os.system("python invariante.py -f "+salida+" -n1 "+str(individuo.var_a)+" -n2 "+str(individuo.var_b)+" -n "+"result.txt")
+		with open('result.txt', 'r') as f:
+			last_line = f.read().splitlines()[-1]
+			last_line = last_line.split(' ')
+			individuo.r2 = last_line[2]
 
-# Selection and elite
+	# Selection and elite
+	population = sorted(population, key=lambda x: x.r2, reverse=True)
+	population_new[0] = population[0]
+	population_new[1] = population[1]
 
-population_new = sorted(population, key=lambda x: x.r2, reverse=True)
+	# Elite and discard zero r2
+	index_descart = population_n-1
+	for i in range(population_n):
+		if population[i].r2 == 0:
+			index_descart = i
+			break
+	for i in range(population_n):
+		if population[i].r2 == 0:
+			population_new[i] = Individuo()
 
-print("********** GEN **********")
-for individuo in population_new:
-	print(individuo.r2)
+	# Crossover
+	for i in range(population_n-2):
+		# Crossover except elite
+		gen_a = individuo[rd.randint(0, index_descart)].var_a
+		gen_b = individuo[rd.randint(0, index_descart)].var_b
+		population_new[i+2].var_a = gen_a
+		population_new[i+2].var_b = gen_b
+
+	# Mutation
+	for i in range(population):
+		population_new.var_a += rd.randrange(-100, 100)/100
+		population_new.var_b += rd.randrange(-100, 100)/100
 
 
-# Crossover
+	print("*************************")
+	print("********** GEN "+ str(gen_i) +" **********")
+	print("*************************")
+	for individuo in population:
+		print(individuo.r2)
 
-
-# Mutation
 
