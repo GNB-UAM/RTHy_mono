7b3b18a7682571f11253ce408c01604580f00bb3
Manuel Reyes <mnrs94@gmail.com>
2021-01-27 11:33:57 +0100

    a

--- genetico.py
+++ genetico.py
@@ -30,8 +30,12 @@ for individuo in population:
 	print(name)
 	os.system("./RTHybrid -xml "+name)
 	os.system("python invariante.py -f "+salida+" -n1 "+str(individuo.var_a)+" -n2 "+str(individuo.var_b)+" -n "+"result.txt")
+	with open("result.csv") as f:
+		f.​readlines()[-1]
+
 	with open('result.txt', 'r') as f:
 		last_line = f.read().splitlines()[-1]
+		last_line = last_line.split(' ')
 		individuo.r2 = last_line[2]
 
 # Selection and elite
