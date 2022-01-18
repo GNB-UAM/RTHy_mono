12ba431718ecc3c244a662050318cb77492a2a92
Manuel Reyes <mnrs94@gmail.com>
2022-01-18 12:48:16 +0100

    clean

--- launch_launch_test.sh
+++
@@ -1,7 +0,0 @@
-echo -e '#!/bin/bash
-#SBATCH --job-name=RTHy_mono
-#SBATCH --output jobs/RTHY_mono_launch_test_0.out
-#SBATCH --error  jobs/RTHY_mono_launch_test_0.err
-/bin/echo Estoy corriendo en el nodo `hostname`
-/bin/echo Empiezo a las `date`
-
